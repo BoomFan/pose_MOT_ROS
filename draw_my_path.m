0ca3ab9319b5dc2db2f215c7b6436dde7de95da3
ROAHM Lab <boomf2011@gmail.com>
2017-02-03 18:29:27 -0500

    draw trajectory

--- draw_my_path.m
+++ draw_my_path.m
@@ -11,8 +11,11 @@
 %     odm = receive(sub_odm)
 clear all
 close all
-    sub_traj = rossubscriber('/segway/pose');
+    sub_traj = rossubscriber('/segway/pose_local');
+    sub_traj_wheel = rossubscriber('/segway/pose_wheel');
+    
     traj = receive(sub_traj);
+    traj_wheel = receive(sub_traj_wheel);
     sub_map = rossubscriber('/map');
     map = receive(sub_map);
     myMap = readBinaryOccupancyGrid(map);
@@ -22,6 +25,12 @@ close all
     trajectory_x(i)=(traj.Cells(i, 1).X);
     trajectory_y(i)=(traj.Cells(i, 1).Y);
     end
+    
+        for i=1:length(traj_wheel.Cells)
+    trajectory_wheel_x(i)=(traj_wheel.Cells(i, 1).X);
+    trajectory_wheel_y(i)=(traj_wheel.Cells(i, 1).Y);
+    end
 % %     plot(traj.Poses(5, 1).Pose.Position.X,traj.Poses(5, 1).Pose.Position.Y) 
 plot(trajectory_x,trajectory_y) 
+plot(trajectory_wheel_x,trajectory_wheel_y) 
 % %     end
